035e53c7f0d785e409a62d1103129e3b594a77bb
Fernando Gomes <feromes@gmail.com>
2025-12-09 13:05:05 -0300

    Instalando o TailWindCSS

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,26 +2,19 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import RotatingCube from "./components/scene/RotatingCube";
-import "./App.css";
 
 function App() {
   return (
-    <div className="app-root">
+    <div className="w-screen h-[100dvh] bg-red-600">
       <Canvas
         camera={{ position: [3, 3, 3], fov: 50 }}
-        dpr={[1, 2]}
+        dpr={[1, 2]} 
+        className="w-full h-full bg-red-600"
       >
-        {/* Cor de fundo */}
         <color attach="background" args={["#020617"]} />
-
-        {/* Luzes */}
         <ambientLight intensity={0.4} />
         <directionalLight position={[5, 5, 5]} intensity={0.8} />
-
-        {/* Cubo girando */}
         <RotatingCube />
-
-        {/* Controles de órbita */}
         <OrbitControls enableDamping />
       </Canvas>
     </div>
